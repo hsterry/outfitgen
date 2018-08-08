f4b712409cdcc8d108e92b77bbb281be067b1e41
unknown <emmejay24@gmail.com>
2018-08-08 10:59:40 -0700

    The Weather API

---
+++ WeatherAPI.js
@@ -0,0 +1,34 @@
+
+// user puts in the zip code allowing the API to find the location and the weather in that location
+// the function name will be :find. b/c it finds the zip cod
+
+function findWeather(){
+  var zipCode = document.getElementById("Get Zip Weather").value;
+
+
+  if(zipCode === ""){
+    alert("You didn't enter a Zip Code!");
+    return;
+  }
+
+  var query = "https://api.openweathermap.org/data/2.5/weather?zip=" +zipCode+ ",us &APPID=e6e087dc78aafce7f5b8c47b9a8fe706";
+
+  query = query.replace(/ /g, "%20")
+  var zipRequest = new XMLHttpRequest();
+  zipRequest.open('GET', query, false);
+
+  zipRequest.send();
+
+  if(zipRequest.readyState != 4 || zipRequestStatus  = 200)
+    window.console.console.error("Request had an error");
+    return;
+    }
+  var zipInformation = JSON.parse(zipRequest.readyState);
+
+
+  //var log = zipInformation [0].latlng[1];
+//var lat = zipInformation[0].latlng[1];
+  //var location = ol.proj.fromLonLat([lon,lat]);
+  var Weather = zipInformation.Weather;
+    return Weather;
+}
