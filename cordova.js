d39c9ba68f238c211972a2d1ab2dd2c8df41281f
hailey <terryhailey@outlook.com>
2018-08-08 10:38:34 -0700

    input_outfit

---
+++ cordova.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function() {
+  document.querySelector('input[type="file"]').addEventListener('change', function() {
+      if (this.files && this.files[0]) {
+          var img = document.querySelector('img');  // ('img')[0]
+          img.src = URL.createObjectURL(this.files[0]); // set src to file url
+          img.onload = imageIsLoaded; // optional onload event listener
+      }
+  });
+});
+function imageIsLoaded(e) { alert(e); }
