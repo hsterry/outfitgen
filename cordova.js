2996a91228e4b1972d2b3efbe7c0e0fa0a20e0a1
hailey <terryhailey@outlook.com>
2018-08-09 11:34:24 -0700

    input_outfit

--- cordova.js
+++ cordova.js
@@ -2,6 +2,6 @@ function aFunction() {
   var photo = document.getElementById('pic').value;
   var newPhoto = document.getElementById("holder");
   newPhoto.src=photo;
-  localStorage.setItem('key', 'photo');
+  localStorage.setItem('key', photo);
   newPhoto.src=localStorage.getItem('key');
 }
